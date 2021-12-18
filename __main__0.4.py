8bb3a0bef82b88c8f7e1f859e8f49a599e5395df
Pedro Paulo Bastos Teixeira <pedro_teixeira@id.uff.br>
2021-12-18 20:34:06 -0300

    acurácia corrigida e implementada validaçã cruzada
    Acurácia recebe como parâmetro agora as matrizes e vetor y real com os seus índices correspondentes de cada classe , corrigindo o anterior, onde era passada a matriz e os vetores inteiros.  A validaçao cruzada recebe as mesmas matrizes e vetores y real.

--- __main__0.4.py
+++ __main__0.4.py
@@ -1,6 +1,7 @@
 __name__='__main__'
 from csv import reader
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
 
 def convert_M_F_to_0_1(vals,key,campo_MF):
     resp=[]
@@ -54,8 +55,6 @@ def get_data_bin_classe_A_notA(arq):
                 else:
                     x_i.append(row_i[i])
             resp.append({'x':x_i,'y':y_i,'y_B_notB':y_i_B_notB,'y_C_or_D':y_i_C_or_D,'yText':y_i_textual})
-
-            #print(x_i,y_i)
     return resp
 
 
@@ -64,7 +63,6 @@ def get_data(arq):
     with open(arq,'r') as FP:
         csv_reader = reader(FP)
         rows=list(csv_reader)
-        #print(rows)
     for k in range(len(rows)):
         if k!=0:
             row_i=rows[k]
@@ -76,49 +74,53 @@ def get_data(arq):
                 else:
                     x_i.append(row_i[i])
             resp.append({'x':x_i,'y':y_i})
-
-            #print(x_i,y_i)
     return resp
 
+#==================IMPORTANDO O ARQUIVO, TRANSFORMANDO EM MATRIZ E SEPARANDO O GRUPO DE TESTE E DE TREINO============
 dt=convert_M_F_to_0_1(get_data_bin_classe_A_notA('C:\\Users\\Public\\iaml\\csv_tratado_binario.csv'),'x',1)
 dt_train=[]
 dt_test=[]
 for i in range(int(0.7*len(dt))):
     dt_train.append(dt[i])
-#print(dt_train)
 print(i,'/',len(dt))
 for i in range(i,len(dt)):
     dt_test.append(dt[i])
-for i in dt_train:
-    print(i)
+#for i in dt_train:
+#    print(i)
+#==================IMPORTANDO O ARQUIVO, TRANSFORMANDO EM MATRIZ E SEPARANDO O GRUPO DE TESTE E DE TREINO============
+
+
+# Apagar os warnings
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.filterwarnings("ignore")
 
 #====================INSTANCIAS LR======================
 LR_A_notA=LogisticRegression()
 LR_B_notB=LogisticRegression()
 LR_C_or_D=LogisticRegression()
 #====================INSTANCIAS LR======================
+
 #====================VETORES Y DE EXEMPLOS S MATRIZ DE EXEMPLOS======================
 y_A_notA=convert_to_matriz_de_exemplos_X(dt_train,'y')
 y_B_notB=convert_to_matriz_de_exemplos_X(dt_train,'y_B_notB')
 y_C_or_D=convert_to_matriz_de_exemplos_X(dt_train,'y_C_or_D')
 X=convert_to_matriz_de_exemplos_X(dt_train,'x')
 #====================VETORES Y DE EXEMPLOS S MATRIZ DE EXEMPLOS======================
+
 #====================VETORES Y DE TESTES S MATRIZ DE TESTES======================
 y_A_notA_real=convert_to_matriz_de_exemplos_X(dt_test,'y')
 y_B_notB_real=convert_to_matriz_de_exemplos_X(dt_test,'y_B_notB')
 y_C_or_D_real=convert_to_matriz_de_exemplos_X(dt_test,'y_C_or_D')
 X_test=convert_to_matriz_de_exemplos_X(dt_test,'x')
 #====================VETORES Y DE TESTES S MATRIZ DE TESTES======================
+
 XX_train=[[]]
 yy_train=[]
 yy_train2=[]
 yy_train3=[]
-
-#XX_test=convert_to_matriz_de_exemplos_X(dt_test,'x')
-#yy_test=convert_to_matriz_de_exemplos_X(dt_test,'y')
-#yy_test2=convert_to_matriz_de_exemplos_X(dt_test,'y_B_notB')
-#yy_test3=convert_to_matriz_de_exemplos_X(dt_test,'y_C_or_D')
 y_test_textual=convert_to_matriz_de_exemplos_X(dt_test,'yText')
+
 #====================TREINANDO======================
 LR_A_notA.fit(X,y_A_notA)
 LR_B_notB.fit(X,y_B_notB)
@@ -145,39 +147,38 @@ for i in range(len(predicoes_A_notA)):
         notA.append(i)
     else: #É CLASSE A, adicionar ao grupo
         A.append(i)
-#print('predicao',list(predicoes_A_notA)[:20])
-#print('index A',list(A)[:20])
-#print('index ¬A',list(notA)[:20])
 
 #-----------------Peneira B ou não B------------------
 X_TEMP=[]
+y_B_notB_real_y=[]
 for i in notA:
     X_TEMP.append(X_test[i])
-    #print(X_test[i],'%%%')
+    # vetor com valores reais correspondentes a matriz X Temp será guardado para acuracia e metricas
+    y_B_notB_real_y.append(y_B_notB_real[i])
 #carregando o novo vetor X pra usar no proximo classificador, já que podemos desconsiderar quem já é A
 predicoes_B_notB=LR_B_notB.predict(X_TEMP)
+X_notA=X_TEMP
 # cada predição p_i corresponde a posição marcado por cada valor do vetor notA
 # p_i é previsão de....        vetor notA
 #  1                    X[1]       1
 #  0                    X[3]       3
 #  0                    X[5]       5
 #  ...
-#print(len(notA),len(X_TEMP),len(predicoes_B_notB))
-#print(list(predicoes_B_notB))
 for i in range(len(predicoes_B_notB)):
     if predicoes_B_notB[i]==0: #NÃO É Classe B
-        #print(i,notA[i])
         notB.append(notA[i])
     else: #É CLASSE B, adicionar ao grupo
         B.append(notA[i])
 
-#print(len(predicoes_B_notB),len(B),len(notB),len(B)+len(notB),len(X_TEMP))
-#print(len(notA),len(X_TEMP))
 # -------------------Peneira C ou D------------------
+y_C_or_D_real_y=[]
 X_TEMP=[]
 for i in notB:
     X_TEMP.append(X_test[i])
+    # vetor com valores reais correspondentes a matriz X Temp será guardado para acuracia e metricas
+    y_C_or_D_real_y.append(y_C_or_D_real[i])
 predicoes_C_or_D=LR_C_or_D.predict(X_TEMP)
+X_notB=X_TEMP
 for i in range(len(predicoes_C_or_D)):
     if predicoes_C_or_D[i]==0: #É D
         #print(i,notA[i])
@@ -217,15 +218,25 @@ for i in range(len(X_test)):
     x=X[i]
     y=y_predicao_textual[i]
     linha=x+[y]
-#    print(linha)
     X_test_com_predicao.append(linha)
 #==================CLASSIFICANDO - PENEIRA DE CLASSIFICADORES=================
 
 print(y_test_textual)
 print(y_predicao_textual)
-#print(y_C_or_D_real)
+
+#=======================MÉTRICAS===========================
+#Acurácia dos classificadores
 print('Acurácias')
-# teria q usar os X_test q usei lá em cima pra cada caso???
-print('A ou ¬A',LR_A_notA.score(X_test,y_A_notA_real))
-print('B ou ¬B',LR_B_notB.score(X_test,y_B_notB_real))
-print('C ou D',LR_C_or_D.score(X_test,y_C_or_D_real))
+print('Acurácia do classificador A ou ¬A',LR_A_notA.score(X_test,y_A_notA_real))
+print('Acurácia do classificador B ou ¬B',LR_B_notB.score(X_notA,y_B_notB_real_y))
+print('Acurácia do classificador C ou D',LR_C_or_D.score(X_notB,y_C_or_D_real_y))
+
+#Validação Cruzada
+print('\nValidação Cruzada')
+mediasA_notA=cross_val_score(LR_A_notA,X_test,y_A_notA_real,cv=5)
+mediasB_notB=cross_val_score(LR_B_notB,X_notA,y_B_notB_real_y,cv=5)
+mediasC_or_D=cross_val_score(LR_C_or_D,X_notB,y_C_or_D_real_y,cv=5)
+print('Validação cruzada para o modelo LR - Classificador A ou ¬A :',sum(mediasA_notA)/len(mediasA_notA))
+print('Validação cruzada para o modelo LR - Classificador B ou ¬B :',sum(mediasB_notB)/len(mediasB_notB))
+print('Validação cruzada para o modelo LR - Classificador C ou D :',sum(mediasC_or_D)/len(mediasC_or_D))
+#=======================MÉTRICAS===========================
